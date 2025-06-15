9c9246770aefa05fe1a36ebaa88afa6c4345c18c
EdenIvgi <edenavgi@gmail.com>
2025-06-15 13:51:23 +0300

    create BookList
    create BookList to render list of BookPreview components

---
+++ Ex-MissBooks/cmps/BookList.jsx
@@ -0,0 +1,18 @@
+import { BookPreview } from './BookPreview.jsx'
+
+export function BookList({ books, onSelectBookId, onRemoveBook }) {
+  return (
+    <ul className="book-list">
+      {books.map(book => (
+        <li key={book.id}>
+          <BookPreview
+            book={book}
+            onSelect={onSelectBookId}
+            onRemove={onRemoveBook}
+          />
+        </li>
+      ))}
+    </ul>
+  )
+}
+
